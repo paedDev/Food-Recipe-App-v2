caf043dcb7357e46a27be8d20280efe6d92b5d5c
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-03-07 19:35:20 +0800

    Initial Commit

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center py-5 mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+      <h2 className="text-2xl font-semibold">
+        <NavLink to={"/"} className="text-black hover-text-gray-700 duration-500">FoodRecipe</NavLink>
+      </h2>
+      <form action="">
+        <input
+          type="text"
+          name="Search"
+          placeholder="Enter Items"
+          className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
+        />
+      </form>
+      <ul className="flex gap-5">
+        <li>
+          <NavLink to={"/"} className="text-black hover-text-gray-700 duration-500">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/Favorites"} className="text-black hover-text-gray-700 duration-500">Favorites</NavLink>
+        </li>
+        
+      </ul>
+    </nav>
+  );
+}
